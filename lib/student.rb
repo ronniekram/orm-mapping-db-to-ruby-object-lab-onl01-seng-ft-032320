d989d67f4750cb8bca16e904e9eb49198bdf66b9
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-19 02:03:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -119,7 +119,7 @@ class Student
       WHERE grade = ?
     SQL
     
-    DB.[:conn].execute(sql).map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
   end 
