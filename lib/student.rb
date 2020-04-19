bb6dddcfb266ed0abbec9d5f7e108a8e61656f3e
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-19 02:02:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -113,7 +113,15 @@ class Student
   end 
   
   def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+      SELECT * 
+      FROM students 
+      WHERE grade = ?
+    SQL
+    
+    DB.[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end 
-  
   
 end
