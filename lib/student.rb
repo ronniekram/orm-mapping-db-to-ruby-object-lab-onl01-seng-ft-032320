7e088bf9381de16dea4d89809da611139dc5946b
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-19 01:47:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -83,7 +83,7 @@ class Student
   
   def self.first_X_students_in_grade_10(number)
     sql = <<-SQL
-      SELECT COUNT(number)
+      SELECT COUNT(?)
       FROM students 
       WHERE grade = 10
       SQL
