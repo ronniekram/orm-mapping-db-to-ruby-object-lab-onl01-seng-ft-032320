56d4da9f80783e634888b891b33f18432c313b5a
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-19 02:08:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -91,9 +91,9 @@ class Student
       WHERE grade = 10
     SQL
       
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-    end
+    DB[:conn].execute(sql) # .map do |row|
+      # self.new_from_db(row)
+    # end
   end
   
     def self.first_student_in_grade_10
@@ -116,9 +116,9 @@ class Student
       WHERE grade = ?
     SQL
     
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-    end
+    DB[:conn].execute(sql) # .map do |row|
+      # self.new_from_db(row)
+    # end
   end 
   
 end
