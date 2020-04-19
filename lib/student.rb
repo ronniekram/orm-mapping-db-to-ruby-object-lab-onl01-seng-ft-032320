fddfe288045466d267982da90bfc954f90f90528
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-19 01:45:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -75,7 +75,7 @@ class Student
   
   def self.students_below_12th_grade
     sql = <<-SQL
-    SELECT COUNT(grade < 12) FROM students
+    SELECT COUNT(grade <= 11) FROM students
     SQL
     
     DB[:conn].execute(sql)
