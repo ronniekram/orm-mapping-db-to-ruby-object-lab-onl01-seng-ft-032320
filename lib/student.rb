7186559e960f7e145d4600d9ebc17702dd5c39dc
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-19 02:11:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -92,7 +92,7 @@ class Student
       LIMIT ?
     SQL
       
-    DB[:conn].execute(sql).map do |row|
+    DB[:conn].execute(sql, x).map do |row|
       self.new_from_db(row)
     end
   end
@@ -110,14 +110,14 @@ class Student
     end.first
   end 
   
-  def self.all_students_in_grade_X(grade)
+  def self.all_students_in_grade_X(x)
     sql = <<-SQL
       SELECT * 
       FROM students 
       WHERE grade = ?
     SQL
     
-    DB[:conn].execute(sql).map do |row|
+    DB[:conn].execute(sql, x).map do |row|
       self.new_from_db(row)
     end
   end 
